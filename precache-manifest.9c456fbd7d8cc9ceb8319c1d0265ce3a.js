8c2d149c55f95c1015d0b93d6f64c4c019a085ce
Manoah <149895ja@gmail.com>
2020-08-20 12:24:52 +0200

    Updates

---
+++ precache-manifest.9c456fbd7d8cc9ceb8319c1d0265ce3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c456d3905e3f44b6c208b7c346ce6ea",
+    "url": "/TemperatureCalcReact/index.html"
+  },
+  {
+    "revision": "2c755042f53112c91155",
+    "url": "/TemperatureCalcReact/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2023c14e6df12eed3b3f",
+    "url": "/TemperatureCalcReact/static/js/2.329f147c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TemperatureCalcReact/static/js/2.329f147c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c755042f53112c91155",
+    "url": "/TemperatureCalcReact/static/js/main.676c1db0.chunk.js"
+  },
+  {
+    "revision": "b4fe8fb6419e705e8be2",
+    "url": "/TemperatureCalcReact/static/js/runtime-main.e61aa5c4.js"
+  }
+]);
